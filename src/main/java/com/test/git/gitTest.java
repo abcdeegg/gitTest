76220e74e174185430fdd46860945a24b955f42c
abcdeegg <206075682@qq.com>
2022-08-29 19:45:55 +0800

    hot-fix second commit

--- src/main/java/com/test/git/gitTest.java
+++ src/main/java/com/test/git/gitTest.java
@@ -6,5 +6,6 @@ public class gitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("fixed");
+        System.out.println("fixed2");
     }
 }
